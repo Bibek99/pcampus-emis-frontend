0dace8389b6c701876a169b375ef14931a14ab38
Bibek Basyal <bibekbasyal22@gmail.com>
2022-02-02 13:52:17 +0545

    dependencies added

--- prettier.config.js
+++ prettier.config.js
@@ -2,6 +2,10 @@ module.exports = {
   arrowParens: 'always',
   singleQuote: true,
   tabWidth: 2,
-  semi: false,
+  semi: true,
   tailwindConfig: './tailwind.config.js',
+  importOrder: ['^react$', '^(?!^react$|^@/|^[./].*', '^@/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifier: true,
+  importOrderParserPlugins: ['jsx', 'typescript'],
 }
