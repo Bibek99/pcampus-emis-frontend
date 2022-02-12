604bdf3e3275014f1c605f30a5333476f51b75e3
Bibek Basyal <bibekbasyal22@gmail.com>
2022-02-12 16:06:14 +0545

    assignment view

--- src/app/contentblocks/Class/AssignmentView.tsx
+++ src/app/contentblocks/Class/AssignmentView.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
+export const AssignmentItem = () => {
+  return (
+    <div className="flex space-x-4 pt-4">
+      <div>Avatar</div>
+      <div>Assignment Details</div>
+    </div>
+  );
+};
+
 export const AssignmentView = () => {
-  return <div>AssignmentView</div>;
+  return (
+    <>
+      <div className="flex flex-col space-y-6 divide-y-2">
+        {[1, 2, 3, 4].map((assignment, index) => (
+          <AssignmentItem key={index} />
+        ))}
+      </div>
+    </>
+  );
 };
